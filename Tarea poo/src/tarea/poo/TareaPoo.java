490fddc8e27d541b2afeb0299b5b8c3bfb2178ce
leilaniegamezc <michellegamezc@gmail.com>
2025-04-22 09:38:44 -0700

    2025 Abr 22 Michelle

--- Tarea poo/src/tarea/poo/TareaPoo.java
+++ Tarea poo/src/tarea/poo/TareaPoo.java
@@ -14,7 +14,10 @@ public class TareaPoo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Tareapo tarea = new Tareapo();
+        tarea.setVisible(true);
+        
+        System.out.println("hmm");
     }
     
 }
